18e1ca239dabec2dbe57b3c5d551c86fda507b06
Terry Cain <terry@terrys-home.co.uk>
2016-01-12 00:52:23 +0000

    Updated packaging to put python razer lib into the python path

--- gui/lib/razer/daemon_dbus.py
+++ gui/lib/razer/daemon_dbus.py
@@ -3,6 +3,9 @@
 import dbus
 
 class DaemonInterface(object):
+    """
+    Interface to the daemon via DBUS
+    """
     def __init__(self):
       try:
         # Load up the DBUS
@@ -29,6 +32,21 @@ class DaemonInterface(object):
         exit()
 
     def set_effect(self, effect_type, p1=1, p2=255, p3=255):
+        """
+        Set effect on keyboard
+
+        :param effect_type: Keyboard effect type
+        :type effect_type: str
+
+        :param p1: Parameter 1
+        :type p1: int
+
+        :param p2: Parameter 2
+        :type p2: int
+
+        :param p3: Parameter 3
+        :type p3: int
+        """
         print("[Daemon] Setting effect: \"{0}\"".format(effect_type))
 
         if effect_type == "breath":
@@ -58,31 +76,47 @@ class DaemonInterface(object):
             # Expects a integer parameter
             # 1 = Wave Right
             # 2 = Wave Left
-            print("[Daemon] Parameters: "+str(p1))
+            print("[Daemon] Parameters: {0}".format(p1))
             self.dbus_driver_effect_object.wave(p1)
         else:
             print("[Daemon] Invalid effect \"{0}\"".format(effect_type))
 
     def set_brightness(self, brightness):
+        """
+        Set keyboard brightness
+
+        :param brightness: Brightness value byte scaled (0-255)
+        :type brightness: int
+        """
         percent = round( (brightness / 255.0) * 100 )
         print("[Daemon] Brightness Set: {0} % ({1}/255)".format(percent, brightness))
         self.dbus_daemon_controls.raw_keyboard_brightness(brightness)
 
-    def marco_keys(self, state):
-        if state:
+    def marco_keys(self, enable):
+        """
+        Enable macro keys on the keyboard
+
+        :param enable: Boolean
+        :type enable: bool
+        """
+        if enable:
             print("[Daemon] Marco Keys: Enabled")
             self.dbus_daemon_controls.enable_macro_keys()
-        elif not state:
+        elif not enable:
             print("[Daemon] Restart the 'razer_bcd' service to disable marco keys.")
         else:
-            print("[Daemon] Invalid parameter for 'MarcoKeys' = {0}".format(state))
+            print("[Daemon] Invalid parameter for 'MarcoKeys' = {0}".format(enable))
 
-    def game_mode(self, state):
-        if state:
+    def game_mode(self, enable):
+        """
+        Enable or disable game mode
+
+        :param enable: Boolean
+        :type enable: bool
+        """
+        if enable:
             print("[Daemon] Game Mode: Enabled")
-            self.dbus_daemon_controls.set_game_mode(1)
-        elif not state:
-            print("[Daemon] Game Mode: Disabled")
-            self.dbus_daemon_controls.set_game_mode(0)
+            self.dbus_daemon_controls.set_game_mode(True)
         else:
-            print("[Daemon] Invalid parameter for 'GameMode' = {0}".format(state))
+            print("[Daemon] Game Mode: Disabled")
+            self.dbus_daemon_controls.set_game_mode(False)
