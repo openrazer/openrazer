3400b4bb15b32217abfc78b65b3fe81aff2f43bb
thekiwi5000 <nzkiwi.5000@gmail.com>
2016-01-28 21:08:26 +0100

    Refactored dynamic.c

--- examples/dynamic.c
+++ examples/dynamic.c
@@ -15,14 +15,14 @@ int main(int argc,char *argv[])
 {
   
   
-  struct razer_chroma *klaw=razer_open();
-  if(!klaw) {
+  struct razer_chroma *kbd=razer_open(NULL,NULL);
+  if(!kbd) {
     printf("failed to initialize!!!\n");
     exit(0);
   }
-  razer_set_custom_mode(klaw);
-  razer_clear_all(klaw->keys);
-  razer_update_keys(klaw, klaw->keys);
+  razer_set_custom_mode(kbd);
+  razer_clear_all(kbd->active_device->leds);
+  razer_update_leds(kbd, kbd->active_device->leds);
   
   struct razer_rgb color;
   struct razer_pos pos;
@@ -42,8 +42,8 @@ int main(int argc,char *argv[])
       color.b = ib;
       pos.x = x;
       pos.y = y;
-      razer_set_key_pos(klaw->keys, &pos, &color);
-      razer_update_keys(klaw, klaw->keys);
+      razer_set_led_pos(kbd->active_device->leds, &pos, &color);
+      razer_update_leds(kbd, kbd->active_device->leds);
       printf("Done!\n");
     }
     if(cmd == 2){
@@ -60,17 +60,32 @@ int main(int argc,char *argv[])
       color.b = ib;
       
       for(unsigned int i = 0; i < strlen(targs); i++){
-	printf("Setting %c...\n", targs[i]);
-	razer_convert_ascii_to_pos(targs[i],&pos);
-	razer_set_key_pos(klaw->keys,&pos,&color);
+      	printf("Setting %c...\n", targs[i]);
+	      razer_convert_ascii_to_pos(targs[i],&pos);
+	      razer_set_led_pos(kbd->active_device->leds,&pos,&color);
       }
-      razer_update_keys(klaw, klaw->keys);
+      razer_update_leds(kbd, kbd->active_device->leds);
       printf("Done!\n");
    
     }
+    
+    if(cmd == 3) {
+      int ir, ig, ib; 
+      printf("Enter r g b: ");
+      scanf("%u %u %u", &ir, &ig, &ib);
+      color.r = ir; 
+      color.g = ig; 
+      color.b = ib; 
+      printf("Setting keys to %u %u %u...\n", ir, ig, ib);
+      razer_set_all(kbd->active_device->leds, &color);
+      razer_update_leds(kbd, kbd->active_device->leds);
+      printf("Done!\n");
+    }
+
+
     if(cmd==0) break;
     
     }
   printf("Exiting...");
-  razer_close(klaw);
+  razer_close(kbd);
 }
