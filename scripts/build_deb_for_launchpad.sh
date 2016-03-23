bb60ad16db40b1e14c581323a4d3503a2e6a6c53
Terry Cain <terry@terrys-home.co.uk>
2016-03-23 12:47:00 +0000

    Made script for packaging things for launchpad

--- scripts/build_deb_for_launchpad.sh
+++ scripts/build_deb_for_launchpad.sh
@@ -14,31 +14,31 @@ ROOT=$(git rev-parse --show-toplevel)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$PACKAGE_VERSION" = "14.04" ] || [ "$PACKAGE_VERSION" = "14.10" ] ; then
-    $PACKAGE_BRANCH="ubuntu14_04_packaging"
+    PACKAGE_BRANCH="ubuntu_14_04_packaging"
 else
-    $PACKAGE_BRANCH="ubuntu15_04_packaging"
+    PACKAGE_BRANCH="ubuntu_15_04_packaging"
 fi
 
 # Go to root of directory
-pushd ${ROOT}
+cd ${ROOT}
 
 # Archive directory
-echo "git archive ${CURRENT_BRANCH} | gzip > ${TEMP_DIR}/${ORIG_TAR}"
+git archive ${CURRENT_BRANCH} | gzip > ${TEMP_DIR}/${ORIG_TAR}
 
-
-exit 1
 # Get debian files
-git checkout ${PACKAGE_BRANCH}
+git checkout ${PACKAGE_BRANCH} &>/dev/null
 git archive ${PACKAGE_BRANCH} | gzip > ${TEMP_DIR}/debian_files.tar.gz
-git checkout ${CURRENT_BRANCH}
+git checkout ${CURRENT_BRANCH} &>/dev/null
 
-pushd ${TEMP_DIR}
+# Merge the two tar (not tar.gz) files
+cd ${TEMP_DIR}
+mkdir razer
+cp ${ORIG_TAR} razer_${VERSION}.orig.tar.gz
+tar xf ${ORIG_TAR} -C razer
+tar xf debian_files.tar.gz -C razer
+cd razer
+dpkg-buildpackage -us -uc
 
+cd ${CWD}
 
-#tar -zchf "razer-1.0.tar.gz" razer_drivers_orig/
-
-#echo -ne "m\n" | bzr dh-make razer 1.0 "razer-1.0.tar.gz"
-
-#cd razer
-#rm debian/README*
-#dpkg-buildpackage -us -uc
+echo "Temp DIR: ${TEMP_DIR}"
