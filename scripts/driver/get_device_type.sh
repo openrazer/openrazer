c42007a8b99b2cee71e9a56cb80dd53b5f32d933
Scott Torborg <storborg@gmail.com>
2016-03-31 19:46:44 -0700

    Add script to get device type

---
+++ scripts/driver/get_device_type.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RAZER_BLACKWIDOW_CHROMA_DEVICES=`ls /sys/bus/hid/devices/ | grep "1532:020[359]"`
+for DEV in $RAZER_BLACKWIDOW_CHROMA_DEVICES
+do 
+	if [ -d "/sys/bus/hid/devices/$DEV/input" ]; then
+		INPUT_DEVS=`ls /sys/bus/hid/devices/$DEV/input`
+		for INPUT_DEV in $INPUT_DEVS
+		do
+			MOUSE=`ls /sys/bus/hid/devices/$DEV/input/$INPUT_DEV/ | grep "mouse"`
+			if [ $MOUSE ]; then
+				#echo "Found Razer LED Device : $DEV"
+				DEVPATH=/sys/bus/hid/devices/$DEV
+                echo "Device Type: $(cat $DEVPATH/device_type)"
+			fi
+		done
+	else
+		#no input directories ? use .0003 as default and try that
+		if [[ "$DEV" == *.0003 ]]; then
+			DEVPATH=/sys/bus/hid/devices/$DEV
+            echo "Device Type: $(cat $DEVPATH/device_type)"
+		fi
+	fi
+done
+
