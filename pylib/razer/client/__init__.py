6e9b59979fe7a6c03db778cb4bcae247fa748830
Terry Cain <terry@terrys-home.co.uk>
2016-10-24 22:52:37 +0100

    Possible launchpad fix

--- pylib/razer/client/__init__.py
+++ pylib/razer/client/__init__.py
@@ -2,7 +2,7 @@ import dbus as _dbus
 from razer.client.device import RazerDeviceFactory as _RazerDeviceFactory
 from razer.client import constants
 
-__version__ = '1.0.16'
+__version__ = '1.0.17'
 
 
 class DaemonNotFound(Exception):
