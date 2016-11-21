230932da496af3bb3d23a3835739b2d47473491c
PetaByteBoy // Milan Pässler <me@petabyteboy.de>
2016-11-21 20:59:52 +0100

    add daemon support for deathadder chroma (#82)
    * Update README.md
    
    * Update mouse.py
    
    * Update mouse.py

--- daemon/razer_daemon/hardware/mouse.py
+++ daemon/razer_daemon/hardware/mouse.py
@@ -115,3 +115,23 @@ class RazerOrochiWired(__RazerDeviceBrightnessSuspend):
     METHODS = ['get_firmware', 'get_matrix_dims', 'has_matrix', 'get_device_name', 'get_device_type_mouse', 'get_brightness', 'set_brightness',
                'set_static_effect', 'set_spectrum_effect', 'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect',
                'set_breath_single_effect', 'set_breath_dual_effect', 'set_idle_time', 'set_low_battery_threshold', 'set_dpi_xy', 'set_scroll_active', 'get_scroll_active']
+
+class RazerDeathadderChroma(__RazerDevice):
+    """
+    Class for the Razer Deathadder Chroma
+    """
+    USB_VID = 0x1532
+    USB_PID = 0x0043
+    HAS_MATRIX = False
+    MATRIX_DIMS = [-1, -1]  # 1 Row, 15 Cols
+    METHODS = ['get_firmware', 'get_matrix_dims', 'has_matrix', 'get_device_name', 'get_device_type_mouse', 'set_logo_active', 'get_logo_active', 'get_brightness', 'set_brightness', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect', 'set_reactive_effect', 'set_none_effect', 
+               'set_breath_single_effect', 'set_custom_effect' ]
+
+    def __init__(self, *args, **kwargs):
+        super(RazerDeathadderChroma, self).__init__(*args, **kwargs)
+
+    def _resume_device(self):
+        self.logger.debug("Deathadder Chroma doesnt have suspend/resume")
+
+    def _suspend_device(self):
+        self.logger.debug("Deathadder Chroma doesnt have suspend/resume")
