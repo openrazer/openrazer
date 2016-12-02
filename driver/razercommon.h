4aecac3a1b4154661eee4867c936662e3eeb0e18
Terry Cain <terry@terrys-home.co.uk>
2016-12-02 23:03:39 +0000

    Added Deathadder Chroma

--- driver/razercommon.h
+++ driver/razercommon.h
@@ -12,7 +12,7 @@
 #ifndef DRIVER_RAZERCOMMON_H_
 #define DRIVER_RAZERCOMMON_H_
 
-#define VERSION "1.0.17"
+#define VERSION "1.1.0"
 
 // Macro to create device files
 #define CREATE_DEVICE_FILE(dev, type) \
