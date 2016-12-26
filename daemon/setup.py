2c8276185b48ee854df192ab84c9e6ba10c611cc
Luca Weiss <luca@z3ntu.xyz>
2016-12-26 13:13:46 +0100

    Add setup.pys for pylib & daemon

---
+++ daemon/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = "razer_daemon",
+    version = "1.1.3",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+)
