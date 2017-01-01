openrazer/openrazer

commit c6b3a8f66e9da62ab422e4ba2c77e92b3c48ec55

Stop killing my battery! (#109)
* Install modules in the correct location

* Get rid of razer_mount

 * Autoloading works fine with the modules in the right place,
   so let's get rid of the janky udev helper script.
 * Use udev rules to encapsulate all the logic instead.

* Stop killing my battery!

 * Because of an issue with signal handling in PyGTK, a workaround was
   in place to yield control from the mainloop every 200ms. On laptops,
   this is not gonna fly, especially when a heavy scan thru sysfs
   happens on every iteration. Powertop consistently had razer-daemon at
   the top of the offender list.
 * To fix this, two changes are made. First, we can listen for device
   hotplug with udev and do add/remove asynchronously. Second, the
   signal hack was removed and replaced with a possibly more heinous
   workaround which allows us to use MainLoop.run() by hooking up
   unix signal handlers and catching some exceptions. With these changes,
   I'm able to idle like a professional again.
 * Pass around pyudev Device objects where necessary and clean up the
   add/remove methods. We still do a full set comparison because of
   wonkiness during suspend.
 * Also added the pyudev dependency to the Debian packaging.

daemon/razer_daemon/daemon.py
@@ -19,15 +19,13 @@ import dbus.mainloop.glib
 import gi
 gi.require_version('Gdk', '3.0')
 import gi.repository
-from gi.repository import GObject
+from gi.repository import GObject, GLib
+from pyudev import Context, Monitor, MonitorObserver
 
 import razer_daemon.hardware
 from razer_daemon.dbus_services.service import DBusService
 from razer_daemon.device import DeviceCollection
 from razer_daemon.misc.screensaver_thread import ScreensaverThread
-
-DAEMON_LOOP_INTERVAL = 0.05
-DEVICE_CHECK_INTERVAL = 200  # Interval seconds to check devices / DAEMON_LOOP_INTERVAL
 
 def daemonize(foreground=False, verbose=False, log_dir=None, console_log=False, run_dir=None, config_file=None, pid_file=None, test_dir=None):
     """
@@ -112,8 +110,11 @@ def daemonize(foreground=False, verbose=False, log_dir=None, console_log=False, 
 
     # Create daemon and run
     daemon = RazerDaemon(verbose, log_dir, console_log, run_dir, config_file, test_dir=test_dir)
+
     try:
         daemon.run()
+    except KeyboardInterrupt:
+        daemon.logger.debug("Exited on user request")
     except Exception as err:
         daemon.logger.exception("Caught exception", exc_info=err)
 
@@ -170,7 +171,14 @@ class RazerDaemon(DBusService):
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         DBusService.__init__(self, self.BUS_PATH, '/org/razer')
 
+        self._init_signals()
         self._main_loop = GObject.MainLoop()
+
+        # Listen for input events from udev
+        self._udev_context = Context()
+        udev_monitor = Monitor.from_netlink(self._udev_context)
+        udev_monitor.filter_by(subsystem='input')
+        self._udev_observer = MonitorObserver(udev_monitor, callback=self._udev_input_event, name='device-monitor')
 
         # Logging
         logging_level = logging.INFO
@@ -223,9 +231,48 @@ class RazerDaemon(DBusService):
         self.sync_effects(self._config.getboolean('Startup', 'sync_effects_enabled'))
         # TODO ======
 
-        # Setup quit signals
-        signal.signal(signal.SIGINT, self.quit)
-        signal.signal(signal.SIGTERM, self.quit)
+    def _init_signals(self):
+        """
+        Heinous hack to properly handle signals on the mainloop. Necessary
+        if we want to use the mainloop run() functionality.
+        """
+        def signal_action(signum):
+            """
+            Action to take when a signal is trapped
+            """
+            self.quit(signum)
+
+        def idle_handler():
+            """
+            GLib idle handler to propagate signals
+            """
+            GLib.idle_add(signal_action, priority=GLib.PRIORITY_HIGH)
+
+        def handler(*args):
+            """
+            Unix signal handler
+            """
+            signal_action(args[0])
+
+        def install_glib_handler(sig):
+            """
+            Choose a compatible method and install the handler
+            """
+            unix_signal_add = None
+
+            if hasattr(GLib, "unix_signal_add"):
+                unix_signal_add = GLib.unix_signal_add
+            elif hasattr(GLib, "unix_signal_add_full"):
+                unix_signal_add = GLib.unix_signal_add_full
+
+            if unix_signal_add:
+                unix_signal_add(GLib.PRIORITY_HIGH, sig, handler, sig)
+            else:
+                print("Can't install GLib signal handler!")
+
+        for sig in signal.SIGINT, signal.SIGTERM, signal.SIGHUP:
+            signal.signal(sig, idle_handler)
+            GLib.idle_add(install_glib_handler, sig, priority=GLib.PRIORITY_HIGH)
 
     def read_config(self, config_file):
         """
@@ -309,17 +356,6 @@ class RazerDaemon(DBusService):
         Loops through the available hardware classes, loops through
         each device in the system and adds it if needs be.
         """
-        if self._test_dir is not None:
-            devices = os.listdir(self._test_dir)
-            dev_path = self._test_dir
-        else:
-            try:
-                devices = os.listdir('/sys/bus/hid/devices')
-            except FileNotFoundError:
-                devices = []
-            finally:
-                dev_path = '/sys/bus/hid/devices'
-
         classes = razer_daemon.hardware.get_device_classes()
 
         if first_run:
@@ -330,16 +366,16 @@ class RazerDaemon(DBusService):
 
                 self.logger.debug(format_str.format(cls.__name__ + ' ', cls.USB_VID, cls.USB_PID))
 
-        device_number = 0
-        for device_id in devices:
+        for device in self._udev_context.list_devices(subsystem='hid'):
+            device_number = 0
+
             for device_class in classes:
-                if device_id in self._razer_devices:
+                if device.sys_name in self._razer_devices:
                     continue
 
-                if device_class.match(device_id, dev_path=dev_path):  # Check it matches sys/ ID format and has device_type file
-                    self.logger.info('Found device.%d: %s', device_number, device_id)
-                    device_path = os.path.join(dev_path, device_id)
-                    razer_device = device_class(device_path, device_number, self._config, testing=self._test_dir is not None)
+                if device_class.match(device.sys_name, device.parent.sys_path):  # Check it matches sys/ ID format and has device_type file
+                    self.logger.info('Found device.%d: %s', device_number, device.sys_name)
+                    razer_device = device_class(device.sys_path, device_number, self._config, testing=self._test_dir is not None)
 
                     # Wireless devices sometimes dont listen
                     count = 0
@@ -350,10 +386,10 @@ class RazerDaemon(DBusService):
                             break
                         count += 1
                     else:
-                        logging.warning("Could not get serial for device {0}. Skipping".format(device_id))
+                        logging.warning("Could not get serial for device {0}. Skipping".format(device.sys_name))
                         continue
 
-                    self._razer_devices.add(device_id, device_serial, razer_device)
+                    self._razer_devices.add(device.sys_name, device_serial, razer_device)
 
                     device_number += 1
 
@@ -361,23 +397,23 @@ class RazerDaemon(DBusService):
         """
         Go through the list of current devices and if they no longer exist then remove them
         """
-        devices_to_remove = []
-
-        for device in self._razer_devices:
+        hid_devices = [dev.sys_name for dev in self._udev_context.list_devices(subsystem='hid')]
+        devices_to_remove = [dev for dev in self._razer_devices if dev not in hid_devices]
+        for device in devices_to_remove:
             if self._test_dir is not None:
-                device_path = os.path.join(self._test_dir, device.device_id)
-            else:
-                device_path = os.path.join('/sys/bus/hid/devices', device.device_id)
-
-            if not os.path.exists(device_path):
                 # Remove from DBus
                 device.dbus.remove_from_connection()
-                devices_to_remove.append(device.device_id)
 
-        for device_id in devices_to_remove:
             # Remove device
-            self.logger.warning("Device %s is missing. Removing from DBus", device_id)
-            del self._razer_devices[device_id]
+            self.logger.warning("Device %s is missing. Removing from DBus", device.device_id)
+            del self._razer_devices[device.device_id]
+
+    def _udev_input_event(self, device):
+        self.logger.debug('Device event [%s]: %s', device.action, device.device_path)
+        if device.action == 'add':
+            self._load_devices()
+        elif device.action == 'remove':
+            self._remove_devices()
 
     def run(self):
         """
@@ -385,37 +421,35 @@ class RazerDaemon(DBusService):
         """
         self.logger.info('Serving DBus')
 
-        # Counter for managing periodic tasks
-        counter = 0
+        # Start listening for device changes
+        self._udev_observer.start()
 
-        # Can't just use mainloop.run() as that blocks and
-        # then signaling exit doesn't work
-        main_loop_context = self._main_loop.get_context()
-        while self._main_loop is not None:
-            if main_loop_context.pending():
-                main_loop_context.iteration()
-            else:
-                time.sleep(DAEMON_LOOP_INTERVAL)
-
-            if counter > DEVICE_CHECK_INTERVAL:  # Time sleeps 1ms so DEVICE_CHECK_INTERVAL is in milliseconds
-                self._remove_devices()
-                self._load_devices()
-                counter = 0
-            counter += 1
+        # Start the mainloop
+        try:
+            self._main_loop.run()
+        except KeyboardInterrupt:
+            self.logger.debug('Shutting down')
 
     def stop(self):
         """
         Wrapper for quit
         """
-        self.quit(None, None)
+        self.quit(None)
 
-    def quit(self, signum, frame):
+    def quit(self, signum):
         """
-        Quit by stopping the main loop and screensaver thread
+        Quit by stopping the main loop, observer, and screensaver thread
         """
         # pylint: disable=unused-argument
-        self.logger.info('Stopping daemon.')
-        self._main_loop = None
+        if signum is None:
+            self.logger.info('Stopping daemon.')
+        else:
+            self.logger.info('Stopping daemon on signal %d', signum)
+
+        self._main_loop.quit()
+
+        # Stop udev monitor
+        self._udev_observer.send_stop()
 
         # Stop screensaver
         self._screensaver_thread.shutdown = True

Makefile
@@ -6,7 +6,7 @@ KERNELDIR?=/lib/modules/$(shell uname -r)/build
 DRIVERDIR?=$(shell pwd)/driver
 
 # Where kernel drivers are going to be installed
-MODULEDIR?=/lib/modules/$(shell uname -r)/kernel/drivers/usb/misc
+MODULEDIR?=/lib/modules/$(shell uname -r)/kernel/drivers/hid
 
 # Python dir
 PYTHONDIR?=$(shell python3 -c 'import sys; print(sys.path[-1])')
@@ -111,7 +111,6 @@ udev_install:
 	@echo -e "\n::\033[34m Installing Razer udev rules\033[0m"
 	@echo "====================================================="
 	install -m 644 -v -D install_files/udev/99-razer.rules $(DESTDIR)/lib/udev/rules.d/99-razer.rules
-	install -m 755 -v -D install_files/udev/razer_mount $(DESTDIR)/lib/udev/razer_mount
 
 udev_uninstall:
 	@echo -e "\n::\033[34m Uninstalling Razer udev rules\033[0m"

debian/control
@@ -33,6 +33,7 @@ Depends: razer-kernel-modules-dkms,
          python3,
          python3-dbus,
          python3-gi,
+         python3-pyudev,
          python3-setproctitle,
          python3-notify2,
          xautomation,

install_files/dkms/dkms.conf
@@ -8,7 +8,7 @@ BUILT_MODULE_LOCATION[2]="driver"
 BUILT_MODULE_NAME[0]="razerkbd"
 BUILT_MODULE_NAME[1]="razermouse"
 BUILT_MODULE_NAME[2]="razerfirefly"
-DEST_MODULE_LOCATION[0]="/kernel/drivers/usb/misc"
-DEST_MODULE_LOCATION[1]="/kernel/drivers/usb/misc"
-DEST_MODULE_LOCATION[2]="/kernel/drivers/usb/misc"
+DEST_MODULE_LOCATION[0]="/kernel/drivers/hid"
+DEST_MODULE_LOCATION[1]="/kernel/drivers/hid"
+DEST_MODULE_LOCATION[2]="/kernel/drivers/hid"
 AUTOINSTALL="yes"

install_files/udev/99-razer.rules
@@ -1,32 +1,36 @@
-# This seems to work
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="011b", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="010d", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="011a", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0203", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0205", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0208", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0209", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0210", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0214", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0216", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="021a", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="021e", ENV{is_razer_kbd}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0220", ENV{is_razer_kbd}="yes"
+ACTION!="add", GOTO="razer_end"
+SUBSYSTEMS=="usb|input|hid", ATTRS{idVendor}=="1532", GOTO="razer_vendor"
+GOTO="razer_end"
 
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0042", ENV{is_razer_mouse}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0043", ENV{is_razer_mouse}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0044", ENV{is_razer_mouse}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0045", ENV{is_razer_mouse}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0046", ENV{is_razer_mouse}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0048", ENV{is_razer_mouse}="yes"
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="002f", ENV{is_razer_mouse}="yes"
+LABEL="razer_vendor"
 
-KERNEL=="????:????:????.????", DRIVERS=="usb", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0c00", ENV{is_razer_firefly}="yes"
+# Keyboards
+ATTRS{idProduct}=="010d|011b|011a|0203|0205|0208|0209|0210|0214|0216|021a|021e|0220", \
+    ENV{ID_RAZER_CHROMA}="1", ENV{RAZER_DRIVER}="razerkbd"
 
-SUBSYSTEM=="hid", DRIVER=="hid-generic", ENV{is_razer_kbd}=="yes", RUN+="razer_mount razerkbd $kernel"
+# Mice
+ATTRS{idProduct}=="002f|0042|0043|0044|0045|0046|0048", \
+    ENV{ID_RAZER_CHROMA}="1", ENV{RAZER_DRIVER}="razermouse"
 
-SUBSYSTEM=="hid", DRIVER=="hid-generic", ENV{is_razer_mouse}=="yes", ATTRS{bInterfaceNumber}=="00", RUN+="razer_mount razermouse $kernel"
+# Firefly
+ATTRS{idProduct}=="0c00", \
+    ENV{ID_RAZER_CHROMA}="1", ENV{RAZER_DRIVER}="razerfirefly"
 
-SUBSYSTEM=="hid", DRIVER=="hid-generic", ENV{is_razer_firefly}=="yes", ATTRS{bInterfaceNumber}=="00", RUN+="razer_mount razerfirefly $kernel"
+# Get out if no match
+ENV{ID_RAZER_CHROMA}!="1", GOTO="razer_end"
 
-KERNEL=="event[0-9]*", GROUP:="plugdev"
+# Set permissions if this is an input node
+SUBSYSTEM=="input|hid", GROUP:="plugdev"
+
+# We're done unless it's the hid node
+SUBSYSTEM!="hid", GOTO="razer_end"
+
+# Rebind if needed
+DRIVER=="hid-generic", \
+    RUN+="/bin/sh -c 'echo $kernel > /sys/bus/hid/drivers/hid-generic/unbind'", \
+    RUN+="/bin/sh -c 'echo $kernel > /sys/bus/hid/drivers/$env{RAZER_DRIVER}/bind'"
+
+# Set permissions on the real driver
+RUN+="/bin/sh -c 'chgrp plugdev /sys/bus/hid/drivers/$env{RAZER_DRIVER}/$kernel/*'"
+
+LABEL="razer_end"

commit 044c982ef2a5fe06b797cee91c19127f7653d4ae

Added orbweaver to daemon
Fixed issue where test mode was broken by c6b3a8f66e9da62ab422e4ba2c77e92b3c48ec55

daemon/razer_daemon/daemon.py
@@ -366,16 +366,31 @@ class RazerDaemon(DBusService):
 
                 self.logger.debug(format_str.format(cls.__name__ + ' ', cls.USB_VID, cls.USB_PID))
 
-        for device in self._udev_context.list_devices(subsystem='hid'):
-            device_number = 0
+        if self._test_dir is not None:
+            device_list = os.listdir(self._test_dir)
+            test_mode = True
+        else:
+            device_list = self._udev_context.list_devices(subsystem='hid')
+            test_mode = False
+
+        device_number = 0
+        for device in device_list:
 
             for device_class in classes:
-                if device.sys_name in self._razer_devices:
+                # Interoperability between generic list of 0000:0000:0000.0000 and pyudev
+                if test_mode:
+                    sys_name = device
+                    sys_path = os.path.join(self._test_dir, device)
+                else:
+                    sys_name = device.sys_name
+                    sys_path = device.sys_path
+
+                if sys_name in self._razer_devices:
                     continue
 
-                if device_class.match(device.sys_name, device.parent.sys_path):  # Check it matches sys/ ID format and has device_type file
-                    self.logger.info('Found device.%d: %s', device_number, device.sys_name)
-                    razer_device = device_class(device.sys_path, device_number, self._config, testing=self._test_dir is not None)
+                if device_class.match(sys_name, sys_path):  # Check it matches sys/ ID format and has device_type file
+                    self.logger.info('Found device.%d: %s', device_number, sys_name)
+                    razer_device = device_class(sys_path, device_number, self._config, testing=self._test_dir is not None)
 
                     # Wireless devices sometimes dont listen
                     count = 0
@@ -386,10 +401,10 @@ class RazerDaemon(DBusService):
                             break
                         count += 1
                     else:
-                        logging.warning("Could not get serial for device {0}. Skipping".format(device.sys_name))
+                        logging.warning("Could not get serial for device {0}. Skipping".format(sys_name))
                         continue
 
-                    self._razer_devices.add(device.sys_name, device_serial, razer_device)
+                    self._razer_devices.add(sys_name, device_serial, razer_device)
 
                     device_number += 1
 

daemon/razer_daemon/dbus_services/dbus_methods/all.py
@@ -112,6 +112,18 @@ def get_device_type_tartarus(self):
     return 'tartarus'
 
 
+@endpoint('razer.device.misc', 'getDeviceType', out_sig='s')
+def get_device_type_orbweaver(self):
+    """
+    Get the device's type
+
+    :return:'tartarus'
+    :rtype: str
+    """
+    self.logger.debug("DBus call get_device_type")
+    return 'orbweaver'
+
+
 @endpoint('razer.device.misc', 'hasMatrix', out_sig='b')
 def has_matrix(self):
     """

daemon/razer_daemon/hardware/device_base.py
@@ -375,7 +375,7 @@ class RazerDevice(DBusService):
         :param device_id: Device ID like 0000:0000:0000.0000
         :type device_id: str
 
-        :param dev_path: Device path. Normally '/sys/bus/hid/devices'
+        :param dev_path: Device path. Normally '/sys/bus/hid/devices/0000:0000:0000.0000'
         :type dev_path: str
 
         :return: True if its the correct device ID
@@ -384,7 +384,7 @@ class RazerDevice(DBusService):
         pattern = r'^[0-9A-F]{4}:' + '{0:04X}'.format(cls.USB_VID) +':' + '{0:04X}'.format(cls.USB_PID) + r'\.[0-9A-F]{4}$'
 
         if re.match(pattern, device_id) is not None:
-            if 'device_type' in  os.listdir(os.path.join(dev_path, device_id)):
+            if 'device_type' in os.listdir(dev_path):
                 return True
 
         return False

daemon/razer_daemon/hardware/keyboards.py
@@ -4,7 +4,7 @@ Keyboards class
 import re
 
 from razer_daemon.hardware.device_base import RazerDeviceBrightnessSuspend as _RazerDeviceBrightnessSuspend
-from razer_daemon.misc.key_event_management import KeyboardKeyManager as _KeyboardKeyManager, TartarusKeyManager as _TartarusKeyManager
+from razer_daemon.misc.key_event_management import KeyboardKeyManager as _KeyboardKeyManager, GamepadKeyManager as _TartarusKeyManager, OrbweaverKeyManager as _OrbweaverKeyManager
 from razer_daemon.misc.ripple_effect import RippleManager as _RippleManager
 
 
@@ -69,6 +69,42 @@ class RazerTartarus(_RazerDeviceBrightnessSuspend):
         self.key_manager.close()
 
 
+class RazerOrbweaver(_RazerDeviceBrightnessSuspend):
+    """
+        Keyboard class
+
+        Has macro functionality and brightness based suspend
+        """
+
+    EVENT_FILE_REGEX = re.compile(r'.*Razer_Orbweaver(-if01)?-event-kbd')
+
+    USB_VID = 0x1532
+    USB_PID = 0x0113
+    HAS_MATRIX = False
+    DEDICATED_MACRO_KEYS = True
+    MATRIX_DIMS = [-1, -1]  # 6 Rows, 22 Cols
+    METHODS = ['get_firmware', 'get_matrix_dims', 'has_matrix', 'get_brightness', 'set_brightness', 'get_device_name', 'get_device_type_orbweaver',
+               'tartarus_get_profile_led_red', 'tartarus_set_profile_led_red', 'tartarus_get_profile_led_green', 'tartarus_set_profile_led_green', 'tartarus_get_profile_led_blue', 'tartarus_set_profile_led_blue',
+               'get_macros', 'delete_macro', 'add_macro', 'tartarus_get_mode_modifier', 'tartarus_set_mode_modifier',
+
+               'bw_set_pulsate', 'bw_set_static']
+
+    def __init__(self, *args, **kwargs):
+        super(RazerOrbweaver, self).__init__(*args, **kwargs)
+        # Methods are loaded into DBus by this point
+
+        self.key_manager = _OrbweaverKeyManager(self._device_number, self.event_files, self, testing=self._testing)
+
+    def _close(self):
+        """
+        Close the key manager
+        """
+        super(RazerOrbweaver, self)._close()
+
+        # TODO look into saving stats in /var/run maybe
+        self.key_manager.close()
+
+
 class RazerBlackWidow2013(_MacroKeyboard):
     """
     Class for the BlackWidow Ultimate 2013
@@ -79,7 +115,7 @@ class RazerBlackWidow2013(_MacroKeyboard):
     USB_PID = 0x011A
     HAS_MATRIX = True
     DEDICATED_MACRO_KEYS = True
-    MATRIX_DIMS = [6, 22] # 6 Rows, 22 Cols
+    MATRIX_DIMS = [6, 22]  # 6 Rows, 22 Cols
     METHODS = ['get_firmware', 'get_matrix_dims', 'has_matrix', 'get_brightness', 'set_brightness', 'get_device_name', 'get_device_type_keyboard', 'get_game_mode', 'set_game_mode', 'set_macro_mode', 'get_macro_mode',
                'get_macro_effect', 'set_macro_effect', 'bw_get_effect', 'bw_set_pulsate', 'bw_set_static', 'get_macros', 'delete_macro', 'add_macro']
 

daemon/razer_daemon/keyboard.py
@@ -64,6 +64,46 @@ TARTARUS_KEY_MAPPING = {
 
 }
 
+#      0   1   2   3   4    5   6   7
+#    ------------------------------------
+# 0 |  1   2   3   4   5        UP
+# 1 |  6   7   8   9   10  LEFT    RIGHT
+# 2 |  11  12  13  14  15  TMB DOWN MS
+# 3 |  16  17  18  19  20
+#
+
+ORBWEAVER_KEY_MAPPING = {
+    '1': (0, 0),
+    '2': (0, 1),
+    '3': (0, 2),
+    '4': (0, 3),
+    '5': (0, 4),
+    'UP': (0, 6),
+
+    '6': (1, 0),
+    '7': (1, 1),
+    '8': (1, 2),
+    '9': (1, 3),
+    '10': (1, 4),
+    'LEFT': (1, 5),
+    'RIGHT': (1, 7),
+
+    '11': (2, 0),
+    '12': (2, 1),
+    '13': (2, 2),
+    '14': (2, 3),
+    '15': (2, 4),
+    'THUMB': (2, 5),
+    'DOWN': (2, 6),
+    'MODE_SWITCH': (2, 7),
+
+    '16': (3, 0),
+    '17': (3, 1),
+    '18': (3, 2),
+    '19': (3, 3),
+    '20': (3, 4)
+}
+
 #
 EVENT_MAPPING = {
     1: 'ESC', 2: '1',  3: '2',  4: '3',  5: '4',  6: '5',  7: '6',  8: '7',  9: '8',
@@ -100,6 +140,35 @@ TARTARUS_EVENT_MAPPING = {
     45: '13',
     46: '14',
     47: '15',
+    56: 'MODE_SWITCH',
+    57: 'THUMB',
+    103: 'UP',
+    105: 'LEFT',
+    106: 'RIGHT',
+    108: 'DOWN',
+}
+
+ORBWEAVER_EVENT_MAPPING = {
+    41: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    15: '6',
+    16: '7',
+    17: '8',
+    18: '9',
+    19: '10',
+    58: '11',
+    30: '12',
+    31: '13',
+    32: '14',
+    33: '15',
+    42: '16',
+    44: '17',
+    45: '18',
+    46: '19',
+    47: '20',
     56: 'MODE_SWITCH',
     57: 'THUMB',
     103: 'UP',

daemon/razer_daemon/misc/key_event_management.py
@@ -25,7 +25,7 @@ import threading
 import time
 
 # pylint: disable=import-error
-from razer_daemon.keyboard import KEY_MAPPING, TARTARUS_KEY_MAPPING, EVENT_MAPPING, TARTARUS_EVENT_MAPPING
+from razer_daemon.keyboard import KEY_MAPPING, TARTARUS_KEY_MAPPING, EVENT_MAPPING, TARTARUS_EVENT_MAPPING, ORBWEAVER_EVENT_MAPPING, ORBWEAVER_KEY_MAPPING
 from .macro import MacroKey, MacroRunner, macro_dict_to_obj
 
 EVENT_FORMAT = '@llHHI'
@@ -203,6 +203,7 @@ class KeyWatcher(threading.Thread):
         :type value: str
         """
         self._shutdown = value
+
 
 class KeyboardKeyManager(object):
     """
@@ -631,9 +632,13 @@ class KeyboardKeyManager(object):
                 #self.temp_key_store_state = False
                 pass
 
-class TartarusKeyManager(KeyboardKeyManager):
+
+class GamepadKeyManager(KeyboardKeyManager):
+    GAMEPAD_EVENT_MAPPING = TARTARUS_EVENT_MAPPING
+    GAMEPAD_KEY_MAPPING = TARTARUS_KEY_MAPPING
+
     def __init__(self, device_id, event_files, parent, use_epoll=True, testing=False):
-        super(TartarusKeyManager, self).__init__(device_id, event_files, parent, use_epoll, testing=testing)
+        super(GamepadKeyManager, self).__init__(device_id, event_files, parent, use_epoll, testing=testing)
 
         self._mode_modifier = False
         self._mode_modifier_combo = []
@@ -687,7 +692,7 @@ class TartarusKeyManager(KeyboardKeyManager):
         try:
             # Convert event ID to key name
 
-            key_name = TARTARUS_EVENT_MAPPING[key_id]
+            key_name = self.GAMEPAD_EVENT_MAPPING[key_id]
             # Key press
 
             # This is the key for storing stats, by generating hour timestamps it will bucket data nicely.
@@ -699,7 +704,7 @@ class TartarusKeyManager(KeyboardKeyManager):
                 # self._logger.debug("Increased key %s", key_name)
             except KeyError:
                 # Create bucket
-                self._stats[storage_bucket] = dict.fromkeys(TARTARUS_KEY_MAPPING, 0)
+                self._stats[storage_bucket] = dict.fromkeys(self.GAMEPAD_KEY_MAPPING, 0)
                 try:
                     # Increment key
                     self._stats[storage_bucket][key_name] += 1
@@ -710,7 +715,7 @@ class TartarusKeyManager(KeyboardKeyManager):
             if self._temp_key_store_active:
                 colour = random_colour_picker(self._last_colour_choice, COLOUR_CHOICES)
                 self._last_colour_choice = colour
-                self._temp_key_store.append((now + self._temp_expire_time, TARTARUS_KEY_MAPPING[key_name], colour))
+                self._temp_key_store.append((now + self._temp_expire_time, self.GAMEPAD_KEY_MAPPING[key_name], colour))
 
             # if self._testing:
             if key_press:
@@ -765,6 +770,12 @@ class TartarusKeyManager(KeyboardKeyManager):
         :type value: bool
         """
         self._mode_modifier = True if value else False
+
+
+class OrbweaverKeyManager(GamepadKeyManager):
+    GAMEPAD_EVENT_MAPPING = ORBWEAVER_EVENT_MAPPING
+    GAMEPAD_KEY_MAPPING = ORBWEAVER_KEY_MAPPING
+
 
 class MediaKeyPress(threading.Thread):
     """

daemon/run_razer_daemon.py
@@ -29,6 +29,7 @@ def parse_args():
 
     return parser.parse_args()
 
+
 def run():
     args = parse_args()
 
@@ -37,7 +38,6 @@ def run():
         os.mkdir(LOG_PATH)
         os.mkdir(os.path.join(BASE_PATH, 'data'))
         shutil.copy(EXAMPLE_CONF, CONF_PATH)
-
 
     daemon_args = {
         'verbose': args.verbose,

pylib/razer/_fake_driver/razerorbweaver.cfg
@@ -1,0 +1,16 @@
+[device]
+dir_name = 0003:1532:0113.0001
+name = Razer Orbweaver
+event = Razer_Orbweaver-event-kbd
+        Razer_Orbweaver-if01-event-kbd
+files = r,version,1.0.0
+        r,device_type,%(name)s
+        r,firmware_version,v1.0
+        r,device_serial,XX0000000006
+        w,matrix_effect_none
+        w,matrix_effect_pulsate
+        w,matrix_effect_static
+        rw,profile_led_red,0
+        rw,profile_led_green,0
+        rw,profile_led_blue,0
+        rw,matrix_brightness,0