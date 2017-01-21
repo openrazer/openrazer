ec6f8fd109280e9a567a2d8cb41293da1359437b
Terry Cain <terry@terrys-home.co.uk>
2017-01-21 21:23:59 +0000

    Added debug lines

--- daemon/openrazer_daemon/misc/ripple_effect.py
+++ daemon/openrazer_daemon/misc/ripple_effect.py
@@ -257,7 +257,7 @@ class BaseEffect(multiprocessing.Process):
     def __init__(self, device_number, dimensions, device_base, event_files, args=None):
         super(BaseEffect, self).__init__()
 
-        self._logger = logging.getLogger('razer.device{0}.ripplethread'.format(device_number))
+        self._logger = logging.getLogger('razer.device{0}.customEffect'.format(device_number))
         self._event_files = event_files
 
         self._effect = None
@@ -719,6 +719,7 @@ class RippleEffect(BaseEffect):
         try:
             key_row, key_col = KEY_MAPPING[EVENT_MAPPING[key_code]]
             self.key_events.append((key_time, key_row, key_col))
+            self._logger.debug("Appending key to list")
         except KeyError:
             pass
 
@@ -731,27 +732,47 @@ class RippleEffect(BaseEffect):
         """
         now = time.time()
 
+        self._logger.debug("1")
+
         expire = now - self.expire_time
+
+        self._logger.debug("2")
 
         while len(self.key_events) > 0 and self.key_events[0][0] < expire:
             self.key_events.pop(0)
+            self._logger.debug("2.1")
+
+        self._logger.debug("3")
 
         # List of radii
         radiuses = []
 
+        self._logger.debug("4")
+
         for event_time, key_row, key_col in self.key_events:
+            self._logger.debug("4.1")
             now_diff = now - event_time
+
+            self._logger.debug("4.2")
 
             # Current radius is based off a time metric
             colour = (0, 255, 0)
             radiuses.append((key_row, key_col, now_diff * 12, colour))
 
+            self._logger.debug("4.3")
+
+        self._logger.debug("5")
         # 1 extra row to do the logo led
         for row in range(0, matrix.rows + 1):
             for col in range(0, matrix.cols):
+
+                self._logger.debug("5.1")
+
                 if row == 0 and col == 20:
                     continue
                 if row == 6:
+                    self._logger.debug("5.2.1")
+
                     if col != 11:
                         continue
                     else:
@@ -762,6 +783,8 @@ class RippleEffect(BaseEffect):
                                 matrix[0, 20] = colour
                                 break
                 else:
+                    self._logger.debug("5.2.1")
+
                     for cirlce_centre_row, circle_centre_col, rad, colour in radiuses:
                         radius = math.sqrt(math.pow(cirlce_centre_row - row, 2) + math.pow(circle_centre_col - col, 2))
                         if rad >= radius >= rad - 1:
