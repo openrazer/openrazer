bcff748d38d3540288b45b8345360d67fa65fa4c
Zander Bolgar <lezed1@users.noreply.github.com>
2017-05-07 05:00:00 -0400

    Add examples of Python client usage (#273)
    * Add a few Python examples
    
    * Add additional information to basic Python example

---
+++ examples/basic_effect.py
@@ -0,0 +1,22 @@
+from razer.client import DeviceManager
+from razer.client import constants as razer_constants
+
+# Create a DeviceManager. This is used to get specific devices
+device_manager = DeviceManager()
+
+
+print("Found {} Razer devices".format(len(device_manager.devices)))
+print()
+
+# Disable daemon effect syncing.
+# Without this, the daemon will try to set the lighting effect to every device.
+device_manager.sync_effects = False
+
+# Iterate over each device and set the wave effect
+for device in device_manager.devices:
+    print("Setting {} to wave".format(device.name))
+
+    # Set the effect to wave.
+    # wave requires a direction, but different effect have different arguments.
+    device.fx.wave(razer_constants.WAVE_LEFT)
+
