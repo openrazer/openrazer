69d36f55b62e326882f55f5449ab93084ae1ffbf
Zander Bolgar <lezed1@gmail.com>
2017-05-06 22:38:00 -0400

    Add a few Python examples

---
+++ examples/custom_zones.py
@@ -0,0 +1,31 @@
+import colorsys
+import random
+
+from razer.client import DeviceManager
+from razer.client import constants as razer_constants
+
+# Create a DeviceManager. This is used to get specific devices
+device_manager = DeviceManager()
+
+
+print("Found {} Razer devices".format(len(device_manager.devices)))
+print()
+
+# Disable daemon effect syncing.
+# Without this, the daemon will try to set the lighting effect to every device.
+device_manager.sync_effects = False
+
+# Helper funciton to generate interesting colors
+def random_color():
+    rgb = colorsys.hsv_to_rgb(random.uniform(0, 1), random.uniform(0.5, 1), 1)
+    return tuple(map(lambda x: int(256 * x), rgb))
+
+# Set random colors for each zone of each device
+for device in device_manager.devices:
+    rows, cols = device.fx.advanced.rows, device.fx.advanced.cols
+
+    for row in range(rows):
+        for col in range(cols):
+            device.fx.advanced.matrix[row, col] = random_color()
+
+    device.fx.advanced.draw()
---
+++ examples/list_devices.py
@@ -0,0 +1,13 @@
+from razer.client import DeviceManager
+
+# Create a DeviceManager. This is used to get specific devices
+device_manager = DeviceManager()
+
+
+print("Found {} Razer devices".format(len(device_manager.devices)))
+print()
+
+
+# Iterate over each device and pretty out some standard information about each
+for device in device_manager.devices:
+    print("{}: type \"{}\"".format(device.name, device.type))
