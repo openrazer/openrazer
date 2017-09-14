ee247b5149c79d3d2ab89e52641190b333240247
Peter Hutterer <peter.hutterer@who-t.net>
2017-09-14 14:23:03 +1000

    daemon: drop the 'already running' check
    python3-daemonize will fail if the pidfile exists already. And if someone
    wants to start multiple daemons with different run_dirs, let them shoot
    themselves in the foot.

--- daemon/openrazer_daemon/daemon.py
+++ daemon/openrazer_daemon/daemon.py
@@ -48,10 +48,6 @@ class RazerDaemon(DBusService):
 
     def __init__(self, verbose=False, log_dir=None, console_log=False, run_dir=None, config_file=None, test_dir=None):
 
-        if self._already_running():
-            print("Daemon already exists. Please stop that one.", file=sys.stderr)
-            sys.exit(-1)
-
         setproctitle.setproctitle('openrazer-daemon')
 
         # Expanding ~ as python doesnt do it by default, also creating dirs if needed
@@ -137,39 +133,6 @@ class RazerDaemon(DBusService):
     @dbus.service.signal('razer.devices')
     def device_added(self):
         self.logger.debug("Emitted Device Added Signal")
-
-    def _already_running(self):
-        """
-        Returns True if a process with our name is already running or false
-        otherwise.
-
-        :rtype: bool
-        """
-
-        # BusyBox pgrep also matches "python3 /usr/bin/openrazer-daemon -Fv" but procps-ng pgrep (version installed on most Linux distros) doesn't match that.
-        # Change the proctitle before checking to something else to support BusyBox pgrep.
-        setproctitle.setproctitle('openrazer-not-yet-daemon')
-
-        # Check if process exists and is not running as current user
-        proc = subprocess.Popen(['pgrep', 'openrazer-daemon'], stderr=subprocess.DEVNULL, stdout=subprocess.PIPE)
-        stdout = proc.communicate()[0]
-
-        # If 0 there are other services running
-        if proc.returncode == 0:
-            current_uid = str(os.getuid())
-
-            # Loop through other running daemon's
-            pids = stdout.decode().strip('\n').split('\n')
-            for pid in pids:
-                # Open status file in /proc to get uid
-                with open('/proc/{0}/status'.format(pid), 'r') as status_file:
-                    for line in status_file:
-                        # Looking for
-                        # Uid:	1000	1000	1000	1000
-                        # If they match current pid, then we have a daemon running as this user
-                        if line.startswith('Uid:') and line.strip('\n').split()[-1] == current_uid:
-                            return True
-        return False
 
     def _create_logger(self, log_dir, log_level, want_console_log):
         """
