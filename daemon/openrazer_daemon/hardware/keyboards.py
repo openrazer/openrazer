b50f13e471b669649523e570cb46a0f2d64d83cf
Luca Weiss <luca@z3ntu.xyz>
2017-10-24 22:49:13 +0200

    0x0224 has 16 columns, not 15
    Fixes #398

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -609,7 +609,7 @@ class RazerBladeLate2016(_MacroKeyboard):
     USB_PID = 0x0224
     HAS_MATRIX = True
     DEDICATED_MACRO_KEYS = False
-    MATRIX_DIMS = [6, 15]
+    MATRIX_DIMS = [6, 16]
     METHODS = ['get_firmware', 'get_matrix_dims', 'has_matrix', 'get_device_name', 'get_device_type_keyboard', 'get_brightness', 'set_brightness', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect',
                'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect',
                'set_custom_effect', 'set_key_row', 'set_starlight_random_effect',
@@ -1018,6 +1018,7 @@ class RazerBladePro2017(_MacroKeyboard):
 
         self.ripple_manager.close()
 
+
 class RazerBladePro2017FullHD(_MacroKeyboard):
     """
     Class for the Razer Blade Pro FullHD (2017)
@@ -1052,6 +1053,7 @@ class RazerBladePro2017FullHD(_MacroKeyboard):
         super(RazerBladePro2017FullHD, self)._close()
 
         self.ripple_manager.close()
+
 
 class RazerBladeStealthLate2017(_MacroKeyboard):
     """
