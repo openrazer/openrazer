d3cab695f827733c723ae08ec163009840b0e0bc
Luca Weiss <luca@z3ntu.xyz>
2017-12-19 19:21:52 +0100

    CI: Install 'coverage' for a new check(?) in pylint 1.8

--- scripts/ci/install-check-deps.sh
+++ scripts/ci/install-check-deps.sh
@@ -6,3 +6,6 @@ apt-get -y install \
 
 pip3 install autopep8
 pip3 install pylint
+
+# pylint errors with 1.8+ on pylib/tests/integration_tests/test_device_manager.py about the missing 'coverage' module
+pip3 install coverage
