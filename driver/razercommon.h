b2563f01a1167216679d2b68ccde0addd9f38658
Luca Weiss <luca@z3ntu.xyz>
2018-02-23 21:54:14 +0100

    Release version 2.2.1

--- driver/razercommon.h
+++ driver/razercommon.h
@@ -14,7 +14,7 @@
 
 #include <linux/usb/input.h>
 
-#define VERSION "2.2.0"
+#define VERSION "2.2.1"
 
 
 // Macro to create device files
--- pylib/setup.py
+++ pylib/setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="openrazer",
-    version="2.2.0",
+    version="2.2.1",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 )
