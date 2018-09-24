00a665a7514f4a20c8f8c4d98bf0a0fd4826af10
Luca Weiss <luca@z3ntu.xyz>
2018-09-25 01:07:39 +0200

    Fix another instance of the same error
    Additionally the `blade_get_logo_active` and `blade_set_logo_active` methods don't exist for two devices, but they never did as far as I can tell

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -664,7 +664,7 @@ class RazerOrnata(_RippleKeyboard):
     WAVE_DIRS = (0, 1)
     MATRIX_DIMS = [6, 22]
     METHODS = ['get_device_type_keyboard', 'set_wave_effect', 'set_static_effect',
-               'set_reactive_effect', 'set_none_effect', 'set_breath_single_effect'
+               'set_reactive_effect', 'set_none_effect', 'set_breath_single_effect',
                'set_custom_effect', 'set_key_row', 'get_game_mode', 'set_game_mode', 'get_macro_mode', 'set_macro_mode', 'set_breath_single_effect',
                'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro',
                'set_starlight_single_effect', 'set_ripple_effect', 'set_ripple_effect_random_colour']
