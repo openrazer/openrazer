0c81d8237f94808b6994d8e8501145dabe769f1f
Luca Weiss <luca@z3ntu.xyz>
2018-11-09 17:09:10 +0100

    pylib: add package metadata to setup.py

--- pylib/setup.py
+++ pylib/setup.py
@@ -6,5 +6,19 @@ from setuptools import setup, find_packages
 setup(
     name="openrazer",
     version="3.4.0",
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+    packages=find_packages(exclude=["tests", "openrazer._fake_driver"]),
+    install_requires=['dbus-python', 'numpy'],
+    author="OpenRazer contributors",
+    description="Library for interacting with the OpenRazer daemon.",
+    license="GPLv2+",
+    url="https://openrazer.github.io/",
+    project_urls={
+        "Bug Tracker": "https://github.com/openrazer/openrazer/issues",
+        "Source Code": "https://github.com/openrazer/openrazer/tree/master/pylib",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
