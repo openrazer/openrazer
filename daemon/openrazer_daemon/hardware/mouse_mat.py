6cde9921e1d97cf973b597822f7022475df80b18
Ethan Chen <intervigil@gmail.com>
2019-03-21 23:51:48 -0700

    goliathus: Add metadata

--- daemon/openrazer_daemon/hardware/mouse_mat.py
+++ daemon/openrazer_daemon/hardware/mouse_mat.py
@@ -24,3 +24,37 @@ class RazerFirefly(__RazerDeviceBrightnessSuspend):
         "side_img": "https://assets.razerzone.com/eeimages/products/21936/fireflycloth-gallery-6.png",
         "perspective_img": "https://assets.razerzone.com/eeimages/products/21936/fireflycloth-gallery-3.png"
     }
+
+class RazerGoliathus(__RazerDeviceBrightnessSuspend):
+    """
+    Class for the Razer Goliathus (2018)
+    """
+    USB_VID = 0x1532
+    USB_PID = 0x0C01
+    HAS_MATRIX = True
+    MATRIX_DIMS = [1, 15]
+    METHODS = ['get_device_type_mousemat', 'set_static_effect', 'set_spectrum_effect',
+               'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect']
+
+    RAZER_URLS = {
+        "top_img": "https://assets.razerzone.com/eeimages/products/21936/rzr_firefly_gallery-2.png",
+        "side_img": "https://assets.razerzone.com/eeimages/products/21936/fireflycloth-gallery-6.png",
+        "perspective_img": "https://assets.razerzone.com/eeimages/products/21936/fireflycloth-gallery-3.png"
+    }
+
+class RazerGoliathusExtended(__RazerDeviceBrightnessSuspend):
+    """
+    Class for the Razer Goliathus Extended (2018)
+    """
+    USB_VID = 0x1532
+    USB_PID = 0x0C02
+    HAS_MATRIX = True
+    MATRIX_DIMS = [1, 15]
+    METHODS = ['get_device_type_mousemat', 'set_static_effect', 'set_spectrum_effect',
+               'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect']
+
+    RAZER_URLS = {
+        "top_img": "https://assets.razerzone.com/eeimages/products/21936/rzr_firefly_gallery-2.png",
+        "side_img": "https://assets.razerzone.com/eeimages/products/21936/fireflycloth-gallery-6.png",
+        "perspective_img": "https://assets.razerzone.com/eeimages/products/21936/fireflycloth-gallery-3.png"
+    }
