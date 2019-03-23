aed1fa642fc76d9f56813724c8f17543f49e6c36
Luca Weiss <luca@z3ntu.xyz>
2019-03-23 08:38:26 +0100

    Add autopep8 to format_source.sh script
    Closes #793

--- scripts/format_source.sh
+++ scripts/format_source.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+set -ex
+
+# C (astyle)
 find driver/ -name "*.c" -o -name "*.h" | xargs astyle --style=linux -n
+
+# Python (autopep8)
+autopep8 --recursive --in-place --max-line-length 500 --ignore E402 .
