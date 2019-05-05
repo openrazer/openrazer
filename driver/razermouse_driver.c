e2d5de3aaf56ac4712131b27062dc289bdd243e6
nightsky30 <6024315+nightsky30@users.noreply.github.com>
2019-05-05 16:31:45 -0500

    Added support for Razer Lancehead Tournament Edition
    Added support for Razer Lancehead Tournament Edition
    
    1532:0060

--- driver/razermouse_driver.c
+++ driver/razermouse_driver.c
@@ -1536,7 +1536,8 @@ static ssize_t razer_attr_write_scroll_mode_breath(struct device *dev, struct de
             break;
 
         case 6: // Dual colour mode
-            report = razer_chroma_extended_matrix_effect_breathing_dual(VARSTORE, SCROLL_WHEEL_LED, (struct razer_rgb*)&buf[0], (struct razer_rgb*)&buf[3]);
+            //report = razer_chroma_extended_matrix_effect_breathing_dual(VARSTORE, SCROLL_WHEEL_LED, (struct razer_rgb*)&buf[0], (struct razer_rgb*)&buf[3]);
+            report = razer_chroma_extended_matrix_effect_breathing_dual(VARSTORE, SCROLL_WHEEL_LED, (struct razer_rgb*)&buf[0], (struct razer_rgb*)&buf[4]);
             break;
 
         default: // "Random" colour mode
