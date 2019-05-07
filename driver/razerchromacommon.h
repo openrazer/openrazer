364926ed1fa8831ccf4794043392b7fcfe337a9c
nightsky30 <6024315+nightsky30@users.noreply.github.com>
2019-05-06 21:21:44 -0500

    lancehead breathing dual test

--- driver/razerchromacommon.h
+++ driver/razerchromacommon.h
@@ -67,6 +67,11 @@ struct razer_report razer_chroma_extended_matrix_effect_reactive(unsigned char v
 struct razer_report razer_chroma_extended_matrix_effect_breathing_random(unsigned char variable_storage, unsigned char led_id);
 struct razer_report razer_chroma_extended_matrix_effect_breathing_single(unsigned char variable_storage, unsigned char led_id, struct razer_rgb *rgb1);
 struct razer_report razer_chroma_extended_matrix_effect_breathing_dual(unsigned char variable_storage, unsigned char led_id, struct razer_rgb *rgb1, struct razer_rgb *rgb2);
+
+//Special
+struct razer_report razer_chroma_extended_matrix_effect_breathing_dual_lanceheadte(unsigned char variable_storage, unsigned char led_id, struct razer_rgb *rgb1, struct razer_rgb *rgb2);
+//Special
+
 struct razer_report razer_chroma_extended_matrix_effect_custom_frame(void);
 struct razer_report razer_chroma_extended_matrix_brightness(unsigned char variable_storage, unsigned char led_id, unsigned char brightness);
 struct razer_report razer_chroma_extended_matrix_get_brightness(unsigned char variable_storage, unsigned char led_id);
