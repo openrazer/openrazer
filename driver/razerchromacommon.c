4c45b053b1c5b3d737d1080c6bb9a686d3882262
nightsky30 <6024315+nightsky30@users.noreply.github.com>
2019-05-06 21:28:33 -0500

    lancehead dual breathing test

--- driver/razerchromacommon.c
+++ driver/razerchromacommon.c
@@ -685,6 +685,22 @@ struct razer_report razer_chroma_extended_matrix_effect_breathing_dual(unsigned 
 
     return report;
 }
+struct razer_report razer_chroma_extended_matrix_effect_breathing_dual_lanceheadte(unsigned char variable_storage, unsigned char led_id, struct razer_rgb *rgb1, struct razer_rgb *rgb2)
+{
+    struct razer_report report = razer_chroma_extended_matrix_effect_base(0x06, variable_storage, led_id, 0x02);
+
+    report.arguments[3] = 0x02;
+    report.arguments[5] = 0x02;
+
+    report.arguments[6] = rgb1->r;
+    report.arguments[7] = rgb1->g;
+    report.arguments[8] = rgb1->b;
+    report.arguments[9] = rgb2->r;
+    report.arguments[10] = rgb2->g;
+    report.arguments[11] = rgb2->b;
+
+    return report;
+}
 
 /**
  * Set the device to "Custom" effect
