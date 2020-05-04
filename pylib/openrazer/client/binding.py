1bb215ec33ab9f41b3efbe83caea925bf00a5315
Mendel Greenberg <chabad360@gmail.com>
2020-05-04 14:31:46 -0700

    added get_actions and value checking

--- pylib/openrazer/client/binding.py
+++ pylib/openrazer/client/binding.py
@@ -3,6 +3,7 @@ Client binding controls
 """
 
 import dbus as _dbus
+import json
 
 
 class Binding(object):
@@ -29,6 +30,33 @@ class Binding(object):
         """
         return self._capabilities.get('binding_' + capability, False)
 
+    def get_actions(self, profile: str, mapping: str, key_code: int) -> dict:
+        """
+        Returns a dict of the actions for the given key
+
+        :param profile: The profile number
+        :type: str
+
+        :param mapping: The map name
+        :type: str
+
+        :param key_code: The key code
+        :type: int
+
+        :return: A dict containing all the actions for the given key
+        :rtype: dict
+
+        :raises ValueError: If parameters are invalid
+        """
+        if not isinstance(profile, str):
+            raise ValueError("profile must be a string")
+        if not isinstance(mapping, str):
+            raise ValueError("mapping must be a string")
+        if not isinstance(key_code, int):
+            raise ValueError("key_code must be an integer")
+
+        return json.loads(self._binding_dbus.getActions(profile, mapping, str(key_code)))
+
     def add_key_action(self, profile: str, mapping: str, key_code: int, value: int) -> bool:
         """
         Add an key press action to the given key
@@ -47,7 +75,17 @@ class Binding(object):
 
         :return: True if success, False otherwise
         :rtype: bool
+
+        :raises ValueError: If parameters are invalid
         """
+        if not isinstance(profile, str):
+            raise ValueError("profile must be a string")
+        if not isinstance(mapping, str):
+            raise ValueError("mapping must be a string")
+        if not isinstance(key_code, int):
+            raise ValueError("key_code must be an integer")
+        if not isinstance(value, int):
+            raise ValueError("value must be an integer")
 
         self._binding_dbus.addAction(profile, mapping, str(key_code), "key", str(value))
 
@@ -66,7 +104,17 @@ class Binding(object):
 
         :param action_id: The action id
         :type: int
+
+        :raises ValueError: If parameters are invalid
         """
+        if not isinstance(profile, str):
+            raise ValueError("profile must be a string")
+        if not isinstance(mapping, str):
+            raise ValueError("mapping must be a string")
+        if not isinstance(key_code, int):
+            raise ValueError("key_code must be an integer")
+        if not isinstance(action_id, int):
+            raise ValueError("action_id must be an integer")
 
         self._binding_dbus.removeAction(profile, mapping, str(key_code), str(action_id))
 
@@ -82,6 +130,14 @@ class Binding(object):
 
         :param key_code: The key code to clear
         :type: int
+
+        :raises ValueError: If parameters are invalid
         """
+        if not isinstance(profile, str):
+            raise ValueError("profile must be a string")
+        if not isinstance(mapping, str):
+            raise ValueError("mapping must be a string")
+        if not isinstance(key_code, int):
+            raise ValueError("key_code must be an integer")
 
         self._binding_dbus.clearActions(profile, mapping, str(key_code))
