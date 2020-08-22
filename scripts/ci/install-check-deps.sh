11195ac5ea23fb57389cfe6b85a090e1b2ebc275
Luca Weiss <luca@z3ntu.xyz>
2020-08-22 12:40:26 +0200

    Fix travis-ci build
    Latest pylint supports isort 5.x.x but isort 5.x.x doesn't support
    Python 3.5 (the version in Ubuntu 16.04) anymore so we have to
    explicitely install isort 4.x.x.

--- scripts/ci/install-check-deps.sh
+++ scripts/ci/install-check-deps.sh
@@ -5,6 +5,7 @@ apt-get -y install \
     python3-pip
 
 pip3 install autopep8
+pip3 install 'isort<5' # isort 5+ requires Python 3.6+
 pip3 install pylint
 
 # pylint errors with 1.8+ on pylib/tests/integration_tests/test_device_manager.py about the missing 'coverage' module
