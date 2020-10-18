195ab2c236efa2c936f4f66047b8833dd84a07fb
Luca Weiss <luca@z3ntu.xyz>
2020-10-18 18:46:10 +0200

    ci: add test to check driver hex casing

--- scripts/ci/test-hex-casing.sh
+++ scripts/ci/test-hex-casing.sh
@@ -11,4 +11,13 @@ if [ ! -z "$out" ]; then
   exit 1
 fi
 
+out=$(git grep "#define USB_DEVICE_ID_" driver/ | cut -d' ' -f3 | sort -f | grep -E '[a-f]')
+
+if [ ! -z "$out" ]; then
+  echo "ERROR: wrong casing of hex number!"
+  echo "Please convert the following hex numbers in driver/ to uppercase:"
+  echo "$out"
+  exit 1
+fi
+
 echo "No issues found."
