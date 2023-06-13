3f7dacfba736611376a1376bc57832ee0afaa9da
Luca Weiss <luca@z3ntu.xyz>
2023-06-13 17:49:16 +0200

    driver: add missing device_remove_file for DeathAdder 2000

--- driver/razermouse_driver.c
+++ driver/razermouse_driver.c
@@ -5929,6 +5929,17 @@ static void razer_mouse_disconnect(struct hid_device *hdev)
             device_remove_file(&hdev->dev, &dev_attr_logo_matrix_effect_breath);
             break;
 
+        case USB_DEVICE_ID_RAZER_DEATHADDER_2000:
+            device_remove_file(&hdev->dev, &dev_attr_dpi);
+            device_remove_file(&hdev->dev, &dev_attr_poll_rate);
+            device_remove_file(&hdev->dev, &dev_attr_scroll_led_brightness);
+            device_remove_file(&hdev->dev, &dev_attr_scroll_led_state);
+            device_remove_file(&hdev->dev, &dev_attr_scroll_led_effect);
+            device_remove_file(&hdev->dev, &dev_attr_logo_led_brightness);
+            device_remove_file(&hdev->dev, &dev_attr_logo_led_state);
+            device_remove_file(&hdev->dev, &dev_attr_logo_led_effect);
+            break;
+
         case USB_DEVICE_ID_RAZER_DIAMONDBACK_CHROMA:
             device_remove_file(&hdev->dev, &dev_attr_matrix_custom_frame);
             device_remove_file(&hdev->dev, &dev_attr_backlight_led_brightness);
