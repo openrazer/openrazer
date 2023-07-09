29bbf766df429ca104a9919fdbd8bdccc1e4424d
Winston Astrachan <winston.astrachan@gmail.com>
2023-07-09 10:32:14 -0400

    Add support for Razer Blade 15 (2023)

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -1864,3 +1864,22 @@ class RazerBlade15AdvancedEarly2022(_RippleKeyboard):
                'set_ripple_effect', 'set_ripple_effect_random_colour']
 
     DEVICE_IMAGE = "https://assets2.razerzone.com/images/pnx.assets/a1ee4c5a780a401444be898fe93ade69/thumbnail-blade15-new-model.png"
+
+
+class RazerBlade152023(_RippleKeyboard):
+    """
+    Class for the Razer Blade 15 (2023)
+    """
+    EVENT_FILE_REGEX = re.compile(r'.*Razer_Blade(-if01)?-event-kbd')
+
+    USB_VID = 0x1532
+    USB_PID = 0x029E
+    HAS_MATRIX = True
+    MATRIX_DIMS = [6, 16]
+    METHODS = ['get_device_type_keyboard', 'get_logo_active', 'set_logo_active', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect',
+               'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect',
+               'set_breath_dual_effect', 'set_custom_effect', 'set_key_row',
+               'set_starlight_random_effect', 'set_starlight_single_effect', 'set_starlight_dual_effect',
+               'set_ripple_effect', 'set_ripple_effect_random_colour']
+
+    DEVICE_IMAGE = "https://dl.razerzone.com/src2/9696/9696-1-en-v2.png"
