openrazer/openrazer

commit 7b8d48e28b22748a3761a58c2d5e137d2f466a22

Move Naga Epic Chroma to effect API in kernel

driver/razermouse_driver.c
@@ -2999,77 +2999,6 @@ static ssize_t razer_attr_read_logo_led_state(struct device *dev, struct device_
 }
 
 /**
- * Common function to handle sysfs write rgb for a given led
- */
-static ssize_t razer_attr_write_led_rgb(struct device *dev, struct device_attribute *attr, const char *buf, size_t count, unsigned char led_id)
-{
-    struct razer_mouse_device *device = dev_get_drvdata(dev);
-    struct razer_report request = {0};
-    struct razer_report response = {0};
-
-    if (count != 3) {
-        printk(KERN_WARNING "razermouse: Scroll wheel LED mode only accepts RGB (3byte)\n");
-        return -EINVAL;
-    }
-
-    request = razer_chroma_standard_set_led_rgb(VARSTORE, led_id, (struct razer_rgb*)&buf[0]);
-    request.transaction_id.id = 0x3F;
-
-    razer_send_payload(device, &request, &response);
-
-    return count;
-}
-
-/**
- * Common function to handle sysfs read rgb for a given led
- */
-static ssize_t razer_attr_read_led_rgb(struct device *dev, struct device_attribute *attr, char *buf, unsigned char led_id)
-{
-    struct razer_mouse_device *device = dev_get_drvdata(dev);
-    struct razer_report request = {0};
-    struct razer_report response = {0};
-
-    request = razer_chroma_standard_get_led_rgb(VARSTORE, led_id);
-    request.transaction_id.id = 0x3F;
-
-    razer_send_payload(device, &request, &response);
-
-    return sprintf(buf, "%u%u%u\n", response.arguments[2], response.arguments[3], response.arguments[4]);
-}
-
-/**
- * Write device file "scroll_led_rgb"
- */
-static ssize_t razer_attr_write_scroll_led_rgb(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
-{
-    return razer_attr_write_led_rgb(dev, attr, buf, count, SCROLL_WHEEL_LED);
-}
-
-/**
- * Read device file "scroll_led_rgb"
- */
-static ssize_t razer_attr_read_scroll_led_rgb(struct device *dev, struct device_attribute *attr, char *buf)
-{
-    return razer_attr_read_led_rgb(dev, attr, buf, SCROLL_WHEEL_LED);
-}
-
-/**
- * Write device file "backlight_led_rgb"
- */
-static ssize_t razer_attr_write_backlight_led_rgb(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
-{
-    return razer_attr_write_led_rgb(dev, attr, buf, count, BACKLIGHT_LED);
-}
-
-/**
- * Read device file "backlight_led_rgb"
- */
-static ssize_t razer_attr_read_backlight_led_rgb(struct device *dev, struct device_attribute *attr, char *buf)
-{
-    return razer_attr_read_led_rgb(dev, attr, buf, BACKLIGHT_LED);
-}
-
-/**
  * Common function to handle sysfs write effect for a given led
  */
 static ssize_t razer_attr_write_led_effect(struct device *dev, struct device_attribute *attr, const char *buf, size_t count, unsigned char led_id)
@@ -3134,22 +3063,6 @@ static ssize_t razer_attr_write_logo_led_effect(struct device *dev, struct devic
 static ssize_t razer_attr_read_logo_led_effect(struct device *dev, struct device_attribute *attr, char *buf)
 {
     return razer_attr_read_led_effect(dev, attr, buf, LOGO_LED);
-}
-
-/**
- * Write device file "backlight_led_effect"
- */
-static ssize_t razer_attr_write_backlight_led_effect(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
-{
-    return razer_attr_write_led_effect(dev, attr, buf, count, BACKLIGHT_LED);
-}
-
-/**
- * Read device file "backlight_led_effect"
- */
-static ssize_t razer_attr_read_backlight_led_effect(struct device *dev, struct device_attribute *attr, char *buf)
-{
-    return razer_attr_read_led_effect(dev, attr, buf, BACKLIGHT_LED);
 }
 
 /**
@@ -4189,7 +4102,6 @@ static DEVICE_ATTR(matrix_effect_breath,      0220, NULL,                       
 static DEVICE_ATTR(scroll_led_brightness,     0660, razer_attr_read_scroll_led_brightness, razer_attr_write_scroll_led_brightness);
 // For old-school led commands
 static DEVICE_ATTR(scroll_led_state,          0660, razer_attr_read_scroll_led_state,      razer_attr_write_scroll_led_state);
-static DEVICE_ATTR(scroll_led_rgb,            0660, razer_attr_read_scroll_led_rgb,        razer_attr_write_scroll_led_rgb);
 static DEVICE_ATTR(scroll_led_effect,         0660, razer_attr_read_scroll_led_effect,     razer_attr_write_scroll_led_effect);
 // For "extended" matrix effects
 static DEVICE_ATTR(scroll_matrix_effect_wave,        0220, NULL,                           razer_attr_write_scroll_matrix_effect_wave);
@@ -4235,8 +4147,6 @@ static DEVICE_ATTR(right_matrix_effect_none,        0220, NULL,                 
 // matrix_brightness should mostly be called backlight_led_brightness (but it's too much work now for old devices)
 static DEVICE_ATTR(backlight_led_brightness,        0660, razer_attr_read_backlight_led_brightness, razer_attr_write_backlight_led_brightness);
 static DEVICE_ATTR(backlight_led_state,             0660, razer_attr_read_backlight_led_state,      razer_attr_write_backlight_led_state);
-static DEVICE_ATTR(backlight_led_rgb,               0660, razer_attr_read_backlight_led_rgb,        razer_attr_write_backlight_led_rgb);
-static DEVICE_ATTR(backlight_led_effect,            0660, razer_attr_read_backlight_led_effect,     razer_attr_write_backlight_led_effect);
 // For "extended" matrix effects
 static DEVICE_ATTR(backlight_matrix_effect_wave,        0220, NULL,                         razer_attr_write_backlight_matrix_effect_wave);
 static DEVICE_ATTR(backlight_matrix_effect_spectrum,    0220, NULL,                         razer_attr_write_backlight_matrix_effect_spectrum);
@@ -5117,13 +5027,15 @@ static int razer_mouse_probe(struct hid_device *hdev, const struct hid_device_id
             CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_dpi);
             CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_poll_rate);
             CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_led_brightness);
-            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_led_state);
-            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_led_rgb);
-            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_led_effect);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_matrix_effect_none);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_matrix_effect_static);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_matrix_effect_breath);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_scroll_matrix_effect_spectrum);
             CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_led_brightness);
-            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_led_state);
-            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_led_rgb);
-            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_led_effect);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_matrix_effect_none);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_matrix_effect_static);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_matrix_effect_breath);
+            CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_backlight_matrix_effect_spectrum);
             CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_charge_low_threshold);
             CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_device_idle_time);
             CREATE_DEVICE_FILE(&hdev->dev, &dev_attr_charge_level);
@@ -5964,13 +5876,15 @@ static void razer_mouse_disconnect(struct hid_device *hdev)
             device_remove_file(&hdev->dev, &dev_attr_dpi);
             device_remove_file(&hdev->dev, &dev_attr_poll_rate);
             device_remove_file(&hdev->dev, &dev_attr_scroll_led_brightness);
-            device_remove_file(&hdev->dev, &dev_attr_scroll_led_state);
-            device_remove_file(&hdev->dev, &dev_attr_scroll_led_rgb);
-            device_remove_file(&hdev->dev, &dev_attr_scroll_led_effect);
+            device_remove_file(&hdev->dev, &dev_attr_scroll_matrix_effect_none);
+            device_remove_file(&hdev->dev, &dev_attr_scroll_matrix_effect_static);
+            device_remove_file(&hdev->dev, &dev_attr_scroll_matrix_effect_breath);
+            device_remove_file(&hdev->dev, &dev_attr_scroll_matrix_effect_spectrum);
             device_remove_file(&hdev->dev, &dev_attr_backlight_led_brightness);
-            device_remove_file(&hdev->dev, &dev_attr_backlight_led_state);
-            device_remove_file(&hdev->dev, &dev_attr_backlight_led_rgb);
-            device_remove_file(&hdev->dev, &dev_attr_backlight_led_effect);
+            device_remove_file(&hdev->dev, &dev_attr_backlight_matrix_effect_none);
+            device_remove_file(&hdev->dev, &dev_attr_backlight_matrix_effect_static);
+            device_remove_file(&hdev->dev, &dev_attr_backlight_matrix_effect_breath);
+            device_remove_file(&hdev->dev, &dev_attr_backlight_matrix_effect_spectrum);
             device_remove_file(&hdev->dev, &dev_attr_charge_low_threshold);
             device_remove_file(&hdev->dev, &dev_attr_device_idle_time);
             device_remove_file(&hdev->dev, &dev_attr_charge_level);

daemon/openrazer_daemon/hardware/mouse.py
@@ -1571,8 +1571,8 @@ class RazerNagaEpicChromaWired(__RazerDevice):
     USB_PID = 0x003E
     METHODS = ['get_firmware', 'get_matrix_dims', 'has_matrix', 'get_device_name', 'get_device_type_mouse', 'get_battery', 'is_charging',
                'get_idle_time', 'set_idle_time', 'get_low_battery_threshold', 'set_low_battery_threshold', 'max_dpi', 'get_dpi_xy', 'set_dpi_xy', 'get_poll_rate', 'set_poll_rate',
-               'set_scroll_active', 'get_scroll_active', 'set_scroll_static_classic', 'set_scroll_pulsate', 'set_scroll_spectrum_classic', 'get_scroll_brightness', 'set_scroll_brightness',
-               'set_backlight_active', 'get_backlight_active', 'set_backlight_static_classic', 'set_backlight_pulsate', 'set_backlight_spectrum_classic', 'get_backlight_brightness', 'set_backlight_brightness']
+               'get_scroll_brightness', 'set_scroll_brightness', 'set_scroll_none', 'set_scroll_static', 'set_scroll_breath_single', 'set_scroll_spectrum',
+               'get_backlight_brightness', 'set_backlight_brightness', 'set_backlight_none', 'set_backlight_static', 'set_backlight_breath_single', 'set_backlight_spectrum']
 
     DEVICE_IMAGE = "https://assets.razerzone.com/eeimages/products/20776/rzrnagaepicchroma_04.png"
 

pylib/openrazer/_fake_driver/razernagaepicchroma.cfg
@@ -4,9 +4,10 @@
 dir_name = 0003:1532:003E.0001
 name = Razer Naga Epic Chroma
 files = rw,backlight_led_brightness,0
-        rw,backlight_led_effect,0
-        rw,backlight_led_rgb,0xFF00FF
-        rw,backlight_led_state,0
+        w,backlight_matrix_effect_breath
+        w,backlight_matrix_effect_none
+        w,backlight_matrix_effect_spectrum
+        w,backlight_matrix_effect_static
         r,charge_level,255
         rw,charge_low_threshold,38
         r,charge_status,1
@@ -18,7 +19,8 @@ files = rw,backlight_led_brightness,0
         r,firmware_version,v1.0
         rw,poll_rate,500
         rw,scroll_led_brightness,0
-        rw,scroll_led_effect,0
-        rw,scroll_led_rgb,0xFF00FF
-        rw,scroll_led_state,0
+        w,scroll_matrix_effect_breath
+        w,scroll_matrix_effect_none
+        w,scroll_matrix_effect_spectrum
+        w,scroll_matrix_effect_static
         r,version,1.0.0

pylib/openrazer/_fake_driver/razernagaepicchromadock.cfg
@@ -4,9 +4,10 @@
 dir_name = 0003:1532:003F.0001
 name = Razer Naga Epic Chroma Dock
 files = rw,backlight_led_brightness,0
-        rw,backlight_led_effect,0
-        rw,backlight_led_rgb,0xFF00FF
-        rw,backlight_led_state,0
+        w,backlight_matrix_effect_breath
+        w,backlight_matrix_effect_none
+        w,backlight_matrix_effect_spectrum
+        w,backlight_matrix_effect_static
         r,charge_level,255
         rw,charge_low_threshold,38
         r,charge_status,1
@@ -18,7 +19,8 @@ files = rw,backlight_led_brightness,0
         r,firmware_version,v1.0
         rw,poll_rate,500
         rw,scroll_led_brightness,0
-        rw,scroll_led_effect,0
-        rw,scroll_led_rgb,0xFF00FF
-        rw,scroll_led_state,0
+        w,scroll_matrix_effect_breath
+        w,scroll_matrix_effect_none
+        w,scroll_matrix_effect_spectrum
+        w,scroll_matrix_effect_static
         r,version,1.0.0

scripts/generate_fake_driver.sh
@@ -2,8 +2,6 @@
 
 declare -A files_metadata=(
     ["backlight_led_brightness"]="rw;0"
-    ["backlight_led_effect"]="rw;0"
-    ["backlight_led_rgb"]="rw;0xFF00FF"
     ["backlight_led_state"]="rw;0"
     ["backlight_matrix_effect_breath"]="w;"
     ["backlight_matrix_effect_none"]="w;"
@@ -98,7 +96,6 @@ declare -A files_metadata=(
     ["right_matrix_effect_wave"]="w;"
     ["scroll_led_brightness"]="rw;0"
     ["scroll_led_effect"]="rw;0"
-    ["scroll_led_rgb"]="rw;0xFF00FF"
     ["scroll_led_state"]="rw;0"
     ["scroll_matrix_effect_blinking"]="w;"
     ["scroll_matrix_effect_breath"]="w;"

commit 6efd65a03cc05b372d4c0726dc2fe4749b9bbb98

driver: Fix effects on Naga Epic Chroma
During refactoring I missed adding the device into the functions that
are used.

Closes #2168

Fixes: 7b8d48e2 ("Move Naga Epic Chroma to effect API in kernel")

driver/razermouse_driver.c
@@ -3095,6 +3095,8 @@ static ssize_t razer_attr_write_matrix_effect_spectrum_common(struct device *dev
     case USB_DEVICE_ID_RAZER_DEATHADDER_CHROMA:
     case USB_DEVICE_ID_RAZER_MAMBA_2012_WIRELESS:
     case USB_DEVICE_ID_RAZER_MAMBA_2012_WIRED:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA_DOCK:
         request = razer_chroma_standard_set_led_state(VARSTORE, led_id, true);
         request.transaction_id.id = 0x3F;
         razer_send_payload(device, &request, &response);
@@ -3285,6 +3287,8 @@ static ssize_t razer_attr_write_matrix_effect_breath_common(struct device *dev, 
     case USB_DEVICE_ID_RAZER_DEATHADDER_3500:
     case USB_DEVICE_ID_RAZER_DEATHADDER_CHROMA:
     case USB_DEVICE_ID_RAZER_DEATHADDER_2013:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA_DOCK:
         if (count != 3) {
             printk(KERN_WARNING "razermouse: Static mode only accepts RGB (3byte)\n");
             return -EINVAL;
@@ -3456,6 +3460,8 @@ static ssize_t razer_attr_write_matrix_effect_static_common(struct device *dev, 
     case USB_DEVICE_ID_RAZER_DEATHADDER_2013:
     case USB_DEVICE_ID_RAZER_MAMBA_2012_WIRELESS:
     case USB_DEVICE_ID_RAZER_MAMBA_2012_WIRED:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA_DOCK:
         request = razer_chroma_standard_set_led_state(VARSTORE, led_id, true);
         request.transaction_id.id = 0x3F;
         razer_send_payload(device, &request, &response);
@@ -3643,6 +3649,8 @@ static ssize_t razer_attr_write_matrix_effect_none_common(struct device *dev, st
     case USB_DEVICE_ID_RAZER_OUROBOROS:
     case USB_DEVICE_ID_RAZER_OROCHI_2013:
     case USB_DEVICE_ID_RAZER_DEATHADDER_2000:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA:
+    case USB_DEVICE_ID_RAZER_NAGA_EPIC_CHROMA_DOCK:
         request = razer_chroma_standard_set_led_state(VARSTORE, led_id, false);
         request.transaction_id.id = 0x3F;
         break;