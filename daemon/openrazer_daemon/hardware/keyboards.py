AndersKlint/openrazer-add-ornata-v3-tkl

Add support for ornata v3 tkl

daemon/openrazer_daemon/hardware/keyboards.py
@@ -787,6 +787,24 @@ class RazerOrnataV3X_Alternate(RazerOrnataV3X):
     USB_PID = 0x02A2
 
 
+class RazerOrnataV3Tenkeyless(_MacroKeyboard):
+    """
+    Class for the Razer Ornata V3 Tenkeyless
+    """
+    EVENT_FILE_REGEX = re.compile(r'.*Razer_Ornata_V3_Tenkeyless(-if01)?-event-kbd')
+
+    USB_VID = 0x1532
+    USB_PID = 0x0294
+    HAS_MATRIX = True
+    MATRIX_DIMS = [1, 1]
+    METHODS = ['get_device_type_keyboard', 'set_static_effect', 'set_spectrum_effect',
+               'set_none_effect', 'set_breath_single_effect', 'set_breath_dual_effect',
+               'set_custom_effect', 'set_key_row', 'get_game_mode', 'set_game_mode', 'get_macro_mode', 'set_macro_mode',
+               'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro']
+
+    DEVICE_IMAGE = "https://dl.razerzone.com/src/6071-1-en-v1.png"
+
+
 class RazerHuntsmanElite(_RippleKeyboard):
     """
     Class for the Razer Huntsman Elite
@@ -794,7 +812,7 @@ class RazerHuntsmanElite(_RippleKeyboard):
     EVENT_FILE_REGEX = re.compile(r'.*Huntsman_Elite(-if01)?-event-kbd')
 
     USB_VID = 0x1532
-    USB_PID = 0x0226
+    USB_PID = 0x02a3
     HAS_MATRIX = True
     WAVE_DIRS = (0, 1)
     MATRIX_DIMS = [9, 22]

openrazer/openrazer

daemon/openrazer_daemon/hardware/keyboards.py
@@ -806,6 +806,25 @@ class RazerOrnataV3X_Alternate(RazerOrnataV3X):
     USB_PID = 0x02A2
 
 
+class RazerOrnataV3Tenkeyless(_RippleKeyboard):
+    """
+    Class for the Razer Ornata V3 Tenkeyless
+    """
+    EVENT_FILE_REGEX = re.compile(r'.*Razer_Ornata_V3_Tenkeyless(-if01)?-event-kbd')
+
+    USB_VID = 0x1532
+    USB_PID = 0x02A3
+    HAS_MATRIX = True
+    MATRIX_DIMS = [1, 8]
+    METHODS = ['get_device_type_keyboard', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect',
+               'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect',
+               'set_custom_effect', 'set_key_row', 'get_game_mode', 'set_game_mode', 'get_macro_mode', 'set_macro_mode',
+               'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro','set_ripple_effect',
+               'set_ripple_effect_random_colour']
+
+    DEVICE_IMAGE = "https://dl.razerzone.com/src2/13038/13038-1-en-v1.png"
+
+
 class RazerHuntsmanElite(_RippleKeyboard):
     """
     Class for the Razer Huntsman Elite