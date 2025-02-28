openrazer/openrazer

commit a32dc5ff3f18f147a3244d3e2e7c43899e4fa7a1

daemon: Be more strict in accepting serial numbers
Proactively check if the serial number matches the pattern of e.g.
PM123456789 and if not the already generate a random one so we don't
crash on completely garbage input.

Also add a warning so people will know at least why a random serial
number is getting used.

Fixes #2322

daemon/openrazer_daemon/hardware/device_base.py
@@ -979,7 +979,15 @@ class RazerDevice(DBusService):
                     time.sleep(0.1)
                     self.logger.debug('getting serial: {0} count:{1}'.format(serial, count))
 
-            if serial == '' or serial == 'Default string' or serial == 'empty (NULL)' or serial == 'As printed in the D cover':
+            # Known bad serials:
+            # - just an empty string
+            # - "Default string"
+            # - "empty (NULL)"
+            # - "As printed in the D cover"
+            # - hex: 01 01 01 01 05 06 07 08 09 0a 0b 0c 0d 0e 0f 10 11 12 13 14 15 16
+            if not re.fullmatch(r"[A-Z]+[\dA-Z]+", serial):
+                self.logger.warning("Invalid serial number found, using a random one.")
+                self.logger.warning("Original value: %s" % serial)
                 serial = 'UNKWN{0:012}'.format(random.randint(0, 4096))
 
             self._serial = serial.replace(' ', '_')

commit a09f59b5e0a2a51b5a11e369412da851942d3f75

daemon: Relax serial number check a bit
While many serial numbers have two uppercase letters and then a mix of
numbers and letters, there seems to be also valid serial numbers which
start with a number. Relax the check to allow any mix of numbers and
uppercase letters. Let's see if that's good enough or we need to tweak
it further.

Closes: #2403
Fixes: a32dc5ff ("daemon: Be more strict in accepting serial numbers")

daemon/openrazer_daemon/hardware/device_base.py
@@ -987,7 +987,7 @@ class RazerDevice(DBusService):
             # - "empty (NULL)"
             # - "As printed in the D cover"
             # - hex: 01 01 01 01 05 06 07 08 09 0a 0b 0c 0d 0e 0f 10 11 12 13 14 15 16
-            if not re.fullmatch(r"[A-Z]+[\dA-Z]+", serial):
+            if not re.fullmatch(r"[\dA-Z]+", serial):
                 self.logger.warning("Invalid serial number found, using a generated one.")
                 self.logger.warning("Original value: %s" % serial)
                 vid, pid = self.get_vid_pid()