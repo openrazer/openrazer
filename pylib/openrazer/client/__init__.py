67cf9173c450e9766bfe9126d050cc385af77624
Luca Weiss <luca@lucaweiss.eu>
2025-03-15 12:09:38 +0100

    Release version 3.10.1

--- pylib/openrazer/client/__init__.py
+++ pylib/openrazer/client/__init__.py
@@ -5,7 +5,7 @@ import dbus as _dbus
 from openrazer.client.device import RazerDeviceFactory as _RazerDeviceFactory
 from openrazer.client import constants
 
-__version__ = '3.10.0'
+__version__ = '3.10.1'
 
 
 class DaemonNotFound(Exception):
