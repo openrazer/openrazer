8a2706dffb3fbb059044e61d9408a1c576dc696a
Jack Greiner <jack@emoss.org>
2025-04-19 05:57:57 -0400

    driver: Replace hrtimer_init() with hrtimer_setup()
    hrtimer_init() was deprecated when hrtimer_setup() was added in Kernel
    6.13. As of Kernel 6.15 hrtimer_init() has been removed.
    
    ifdef's the new function for Kernel 6.13+ and keep the original code for
    compatibility with older kernels.
    
    For context see the following commits:
    - https://github.com/torvalds/linux/commit/908a1d775422ba2e27a5e33d0c130b522419e121
    - https://github.com/torvalds/linux/commit/9779489a31d77a7b9cb6f20d2d2caced4e29dbe6

--- driver/razermouse_driver.c
+++ driver/razermouse_driver.c
@@ -4938,8 +4938,12 @@ static void razer_mouse_init(struct razer_mouse_device *dev, struct usb_interfac
     dev->da3_5g.poll = 1; // Poll rate 1000
 
     // Setup tilt wheel HWHEEL emulation
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+    hrtimer_setup(&dev->repeat_timer, wheel_tilt_repeat, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+#else
     hrtimer_init(&dev->repeat_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
     dev->repeat_timer.function = wheel_tilt_repeat;
+#endif
     dev->tilt_hwheel = 1;
     dev->tilt_repeat_delay = 250;
     dev->tilt_repeat = 33;
