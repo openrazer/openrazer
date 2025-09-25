fb30cf90502d3ef52d491c7b0e7827fd17446746
0lini <72492483+0lini@users.noreply.github.com>
2025-09-25 22:24:00 +0200

    Testing

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -2227,14 +2227,16 @@ class RazerBlackWidowV4Pro75PCTWired(_RippleKeyboard):
     HAS_MATRIX = True
     WAVE_DIRS = (1, 2)
     MATRIX_DIMS = [8, 23]
-    POLL_RATES = [125, 250, 500, 1000]
-    METHODS = ['get_device_type_keyboard', 'set_wave_effect', 'set_wheel_effect', 'set_static_effect', 'set_spectrum_effect',
+    POLL_RATES = [125, 250, 500, 1000, 2000, 4000]
+    METHODS = ['get_device_type_keyboard', 'set_wave_effect',  'set_static_effect', 'set_spectrum_effect',
                'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect',
                'set_custom_effect', 'set_key_row', 'get_game_mode', 'set_game_mode', 'get_macro_mode', 'set_macro_mode',
                'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro',
                'set_starlight_random_effect', 'set_starlight_single_effect', 'set_starlight_dual_effect',
                'get_poll_rate', 'set_poll_rate', 'get_supported_poll_rates',
-               'set_ripple_effect', 'set_ripple_effect_random_colour',
+               # TODO
+               # 
+               #'set_wheel_effect', 'set_ripple_effect', 'set_ripple_effect_random_colour',
                # Battery
                'get_battery', 'is_charging']
 
