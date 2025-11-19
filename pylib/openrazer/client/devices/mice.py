1fad7d9759a780fde4cdeb01ed3f4ba3d3940cc6
Luca Weiss <luca@z3ntu.xyz>
2025-11-19 13:21:55 +0100

    pylib: Remove unused imports

--- pylib/openrazer/client/devices/mice.py
+++ pylib/openrazer/client/devices/mice.py
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-import dbus as _dbus
-
 from openrazer.client.devices import RazerDevice as __RazerDevice
 from openrazer.client.macro import RazerMacro as _RazerMacro
 
